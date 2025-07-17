da6a59bf259a731edfe78565d55867b7819ce93d
IvanNugiban <aa1245541@gmail.com>
2025-07-17 21:38:58 +0200

    Added map dynamic resizing and other settings

--- src/Map.tsx
+++ src/Map.tsx
@@ -8,6 +8,20 @@ interface MapProps {
 
 const Map = ({ plugin }: MapProps) => {
 	const [countries, setCountries] = useState<Set<string>>(new Set(plugin.data.visitedCountries));
+	const ref = useRef<HTMLDivElement>(null);
+	const [size, setSize] = useState({ width: 0, height: 0 });
+
+	useEffect(() => {
+		if (!ref.current) return;
+
+		const observer = new ResizeObserver(([entry]) => {
+			const { width, height } = entry.contentRect;
+			setSize({ width, height });
+		});
+
+		observer.observe(ref.current);
+		return () => observer.disconnect();
+	}, []);
 
 	// Update and save plugin data when countries change
 	useEffect(() => {
@@ -16,8 +30,8 @@ const Map = ({ plugin }: MapProps) => {
 	}, [countries]);
 
 	return (
-		<div className={`countries-map`}>
-				<MapSvg countries={countries} setCountries={setCountries} />
+		<div ref={ref} className={`countries-map`}>
+				<MapSvg countries={countries} setCountries={setCountries} size={size} />
 		</div>
 	);
 };
